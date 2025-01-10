78e6f2bb084b4fdd90dfc78a108fdf1aebce1adc
DaniilTR <qwertyasd.228336@gmail.com>
2025-01-10 18:37:55 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let a = 2 + 2;
+
+switch (a) {
+  case 3:
+    alert( 'Маловато' );
+    break;
+  case 4:
+    alert( 'В точку!' );
+    break;
+  case 5:
+    alert( 'Перебор' );
+    break;
+  default:
+    alert( "Нет таких значений" );
+}
+let c = "1";
+let b = 0;
+
+switch (+a) {
+  case b + 1:
+    alert("Выполнится, т.к. значением +a будет 1, что в точности равно b+1");
+    break;
+
+  default:
+    alert("Это не выполнится");
+}
